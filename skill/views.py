ff24b2da9576702617787777dcb7512c8d687c69
dhananjay <dhananjay232biyani@gmail.com>
2023-11-11 22:40:03 +0530

    feat: add skill views

--- skill/views.py
+++ skill/views.py
@@ -7,47 +7,150 @@ from skill.serializers import (
     UserSkillSerializer
 )
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from skill.serializers import UserSkillSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class SkillViewSet(ModelViewSet):
     serializer_class = SkillSerializer
     queryset = Skill.objects.all()
+    permission_classes = [IsAuthenticated,]
     
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            return super().create(request, *args, **kwargs)
+        else:
+            return Response({"error": "You are not authorized to create a skill"}, status=status.HTTP_401_UNAUTHORIZED)
     
     def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            return super().retrieve(request, *args, **kwargs)
+        else:
+            return Response({"error": "You are not authorized to view a skill"}, status=status.HTTP_401_UNAUTHORIZED)
     
     def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            if current_user.privilege == '1' or current_user.is_superuser:
+                return super().update(request, *args, **kwargs)
+            elif current_user.privilege in [2,3,4]:
+                skill = Skill.objects.get(id=kwargs['pk'])
+                if skill.createdByUser == current_user:
+                    return super().update(request, *args, **kwargs)
+                else:
+                    return Response({"error": "You are not authorized to update the skills"}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({"error": "You are not authorized to update the skills"}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"error": "Your account is not active. Please contact admin"}, status=status.HTTP_401_UNAUTHORIZED)
     
     def partial_update(self, request, *args, **kwargs):
-        return super().partial_update(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            if current_user.privilege == '1' or current_user.is_superuser:
+                return super().update(request, *args, **kwargs)
+            elif current_user.privilege in [2,3,4]:
+                skill = Skill.objects.get(id=kwargs['pk'])
+                if skill.createdByUser == current_user:
+                    return super().update(request, *args, **kwargs)
+                else:
+                    return Response({"error": "You are not authorized to update the skills"}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({"error": "You are not authorized to update the skills"}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"error": "Your account is not active. Please contact admin"}, status=status.HTTP_401_UNAUTHORIZED)
     
     def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
+        user = request.user
+        if user.is_active:
+            if user.privilege in ['1','2','3','4'] or user.is_superuser:
+                skill = Skill.objects.get(id=kwargs['id'])
+                if skill.createdByUser == user or user.privilege == '1':
+                    return Response({'message': 'Opportunity deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+                else:
+                    return Response({'error': 'You are not authorized to delete opportunities'}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({'error': 'You are not authorized to delete opportunities'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({'error': 'You are not authorized to delete opportunities'}, status=status.HTTP_401_UNAUTHORIZED)
     
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            return super().list(request, *args, **kwargs)
+        else:
+            return Response({"error": "You are not authorized to view skills"}, status=status.HTTP_401_UNAUTHORIZED)
     
 class UserSkillViewSet(ModelViewSet):
     serializer_class = UserSkillSerializer
     queryset = UserSkill.objects.all()
     
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            return super().create(request, *args, **kwargs)
+        else:
+            return Response({"error": "You are not authorized to create a skill"}, status=status.HTTP_401_UNAUTHORIZED)
     
     def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            return super().retrieve(request, *args, **kwargs)
+        else:
+            return Response({"error": "You are not authorized to view a skill"}, status=status.HTTP_401_UNAUTHORIZED)
     
     def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            if current_user.privilege == '1' or current_user.is_superuser:
+                return super().update(request, *args, **kwargs)
+            elif current_user.privilege in [2,3,4]:
+                skill = Skill.objects.get(id=kwargs['pk'])
+                if skill.createdByUser == current_user:
+                    return super().update(request, *args, **kwargs)
+                else:
+                    return Response({"error": "You are not authorized to update the skills"}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({"error": "You are not authorized to update the skills"}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"error": "Your account is not active. Please contact admin"}, status=status.HTTP_401_UNAUTHORIZED)
     
     def partial_update(self, request, *args, **kwargs):
-        return super().partial_update(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            if current_user.privilege == '1' or current_user.is_superuser:
+                return super().update(request, *args, **kwargs)
+            elif current_user.privilege in [2,3,4]:
+                skill = Skill.objects.get(id=kwargs['pk'])
+                if skill.createdByUser == current_user:
+                    return super().update(request, *args, **kwargs)
+                else:
+                    return Response({"error": "You are not authorized to update the skills"}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({"error": "You are not authorized to update the skills"}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"error": "Your account is not active. Please contact admin"}, status=status.HTTP_401_UNAUTHORIZED)
     
     def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
+        user = request.user
+        if user.is_active:
+            if user.privilege in ['1','2','3','4'] or user.is_superuser:
+                skill = Skill.objects.get(id=kwargs['id'])
+                if skill.createdByUser == user or user.privilege == '1':
+                    return Response({'message': 'Opportunity deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+                else:
+                    return Response({'error': 'You are not authorized to delete opportunities'}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({'error': 'You are not authorized to delete opportunities'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({'error': 'You are not authorized to delete opportunities'}, status=status.HTTP_401_UNAUTHORIZED)
     
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        current_user = request.user
+        if current_user.isActive:
+            return super().list(request, *args, **kwargs)
+        else:
+            return Response({"error": "You are not authorized to view skills"}, status=status.HTTP_401_UNAUTHORIZED)
